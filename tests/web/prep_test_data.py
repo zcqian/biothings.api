62f6c0ee953c26f9eee4ff773ea180f25c8e4834
Zhongchao Qian <zqian@scripps.edu>
2021-04-19 13:36:11 -0700

    Update prep_test_data.py

--- tests/web/prep_test_data.py
+++ tests/web/prep_test_data.py
@@ -1,34 +1,69 @@
+import logging
 import argparse
 import os
 import json
 
+from pprint import pprint
+from typing import Any, Dict, Optional
+
 from elasticsearch import Elasticsearch
+
+
+def get_fields(d: Dict[str, Any], parents: Optional[list] = None):
+    if not parents:
+        parents = []
+    for k, v in d.items():
+        field_name = '.'.join(parents + [k])
+        if k.startswith('_') or k in ['all']:
+            continue
+        if isinstance(v, dict):
+            if 'properties' in v:
+                yield from get_fields(v['properties'], parents + [k])
+            elif 'type' in v:  # okay when the field name is 'type'
+                yield field_name
+        else:
+            logging.warning("unable to handle %s: %s", field_name, v)
 
 
 def main(args: argparse.Namespace):
     client = Elasticsearch(args.host)
     os.makedirs(args.output, exist_ok=True)
     # dump mapping
+    setting = client.indices.get(args.index)
+    if len(setting) != 1:
+        logging.warning("%s is alias with more than one index?", args.index)
+    setting = setting[next(iter(setting.keys()))]
+    setting["settings"]["index"] = {
+        "analysis": setting["settings"]["index"]["analysis"]
+    }
     with open(os.path.join(args.output, 'data.json'), 'w') as file:
-        setting = client.indices.get(args.index)[args.index]
-        setting["settings"]["index"] = {
-            "analysis": setting["settings"]["index"]["analysis"]}
         json.dump(setting, file, indent=2)
+    # assume only one doc type
+    mappings = setting['mappings']
+    mappings = mappings[next(iter(mappings.keys()))]
+    query = {
+        'query': {
+            'bool': {
+                'must': []
+            }
+        },
+        'stored_fields': []
+    }
+    all_fields = list(get_fields(mappings['properties']))
+    # two things:
+    #  1. this gets nowhere,
+    #     throw in a couple of fields and search gets no hits
+    #  2. too_many_clauses: maxClauseCount is set to 1024
+    for field_name in all_fields:
+        query['query']['bool']['must'].append(
+            {
+                'exists': {'field': field_name}
+            }
+        )
+    result = client.search(index=args.index, body=query)
+    pprint(query)
+    pprint(result)
 
-    #
-    query_ids = []
-    with open(args.input, 'r') as input_file:
-        for line in input_file:
-            try:
-                q = json.loads(line)
-            except json.JSONDecodeError:
-                # I was thinking maybe we can have something like
-                #  field:value, regex:field:.*, !field, !field:value, etc.
-                #  but it turns out that it would be too complex right now
-                query_ids.append(set())  # empty
-                continue
-
-    # pull _id's from ES
 
     # find minimal set of _id's
     # minimal hitting set is NP-complete so this is just an approximation
